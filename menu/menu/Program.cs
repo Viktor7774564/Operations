fbc38d5882a79b1c5e4a65363f06e5ca7e43c932
Viktor7774564 <vitok.joy@gmail.com>
2022-01-20 23:19:54 +0500

    sec proj

---
+++ menu/menu/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b = 0;
+            Console.WriteLine("Выберите одну из операций");
+            Console.WriteLine("- ввести А");
+            Console.WriteLine("- ввести В");
+            Console.WriteLine("- выполнить операцию “+”");
+            Console.WriteLine("- выполнить операцию “-”");
+            Console.WriteLine("- выполнить операцию “*”");
+            Console.WriteLine("- выполнить операцию “/”");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n) 
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                   
+                    break;
+                case 3:
+                   
+                    break;
+                case 4:
+                   
+                    break;
+                case 5:
+                   
+                    break;
+                case 6:
+                   
+                    break;
+            }
+        }
+    }
+}
